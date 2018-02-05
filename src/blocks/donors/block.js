c234cf4be0609da39f35ecdb87be836227cd8694
Eric Daams <eric@164a.com>
2018-02-05 12:21:21 +0930

    Added extra settings to donors block. #485. Last piece of the puzzle is the preview.

--- src/blocks/donors/block.js
+++ src/blocks/donors/block.js
@@ -32,10 +32,12 @@ class CharitableDonorsBlock extends Component {
 		super( ...arguments );
 
         this.toggleDisplayDonorName = this.toggleDisplayDonorName.bind( this );
+        this.toggleDisplayDonorAmount = this.toggleDisplayDonorAmount.bind( this );
         this.toggleDisplayDonorLocation = this.toggleDisplayDonorLocation.bind( this );
         this.toggleDisplayDonorAvatar = this.toggleDisplayDonorAvatar.bind( this );
+        this.toggleDistinctDonors = this.toggleDistinctDonors.bind( this );
     }
-    
+
     toggleDisplayDonorName() {
 		const { displayDonorName } = this.props.attributes;
 		const { setAttributes } = this.props;
@@ -57,57 +59,99 @@ class CharitableDonorsBlock extends Component {
 		setAttributes( { displayDonorAvatar: ! displayDonorAvatar } );
     }
     
+    toggleDisplayDonorAmount() {
+		const { displayDonorAmount } = this.props.attributes;
+		const { setAttributes } = this.props;
+
+		setAttributes( { displayDonorAmount: ! displayDonorAmount } );
+    }
+    
+    toggleDistinctDonors() {
+		const { distinctDonors } = this.props.attributes;
+		const { setAttributes } = this.props;
+
+		setAttributes( { distinctDonors: ! distinctDonors } );
+    }
+
     render() {
-        const setCampaign = ( campaign ) => this.props.setAttributes( { campaign: campaign } );
 		const { attributes, focus, setAttributes } = this.props;
-        const { number, orderBy, campaign, displayDonorName, displayDonorLocation, displayDonorAvatar } = attributes;
+        const { number, orderBy, campaign, distinctDonors, orientation, displayDonorAmount, displayDonorName, displayDonorLocation, displayDonorAvatar } = attributes;
         
         const inspectorControls = focus && (
             <InspectorControls key="inspector" description={ __( 'Configure' ) }>
-                <PanelBody title={ __( 'Filter & Sort' ) }>
-                    <PanelRow>
-                        <RangeControl
-                            key="filter-panel-number-control"
-                            label={ __( 'Number of donors' ) }
-                            value={ number }
-                            onChange={ ( value ) => setAttributes( { number: value } ) }
-                            min="-1"
-                            max="999"
-                        />
-                    </PanelRow>
-                    <PanelRow>
-                        <CampaignSelect
-                            key="filter-panel-campaign-select"
-                            label={ __( 'Campaign' ) }
-                            selectedCampaign={ campaign }
-                            onChange={ setCampaign }
-                        />
-                    </PanelRow>
+                <RangeControl
+                    key="number-control"
+                    label={ __( 'Number of donors' ) }
+                    value={ number }
+                    onChange={ ( value ) => setAttributes( { number: value } ) }
+                    min="-1"
+                    max="999"
+                />
+                <CampaignSelect
+                    key="campaign-select"
+                    label={ __( 'Campaign' ) }
+                    withOptions={ [
+                        {
+                            label: __( 'All Campaigns' ),
+                            value: 'all',
+                        }
+                    ] }
+                    selectedOption={ campaign }
+                    onChange={ ( value ) => this.props.setAttributes( { campaign: value } ) }
+                />
+                <SelectControl
+                    key="orderby-select"
+                    label={ __( 'Order by' ) }
+                    value={ orderBy }
+                    options={ [
+                        {
+                            label: __( 'Most recent' ),
+                            value: 'recent',
+                        },
+                        {
+                            label: __( 'Amount donated' ),
+                            value: 'amount',
+                        },
+                    ] }
+                    onChange={ ( value ) => setAttributes( { orderBy: value } ) }
+                />
+                <ToggleControl
+                    key="distinct-donors-toggle"
+                    label={ __( 'Only show unique donors' ) }
+                    checked={ distinctDonors }
+                    onChange={ this.toggleDistinctDonors }
+                />
+                <PanelBody title={ __( 'Display Settings' ) }>
                     <PanelRow>
                         <SelectControl
-                            key="filter-panel-orderby-select"
-                            label={ __( 'Order by' ) }
-                            value={ orderBy }
+                            key="orientation-select"
+                            label={ __( 'Orientation' ) }
+                            value={ orientation }
                             options={ [
                                 {
-                                    label: __( 'Most recent' ),
-                                    value: 'recent',
+                                    label: __( 'Vertical' ),
+                                    value: 'vertical',
                                 },
                                 {
-                                    label: __( 'Amount donated' ),
-                                    value: 'amount',
+                                    label: __( 'Horizontal' ),
+                                    value: 'horizontal',
                                 },
                             ] }
-                            onChange={ ( value ) => setAttributes( { orderBy: value } ) }
+                            onChange={ ( value ) => setAttributes( { orientation: value } ) }
                         />
                     </PanelRow>
-                </PanelBody>
-                <PanelBody title={ __( 'Display Settings' ) }>
                     <PanelRow>
                         <ToggleControl
                             label={ __( 'Display the name of the donor' ) }
                             checked={ displayDonorName }
                             onChange={ this.toggleDisplayDonorName }
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <ToggleControl
+                            label={ __( 'Display the amount donated by the donor' ) }
+                            checked={ displayDonorAmount }
+                            onChange={ this.toggleDisplayDonorAmount }
                         />
                     </PanelRow>
                     <PanelRow>
